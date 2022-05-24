1fdb46c5a7a12394c38509d1ef9c74e8f3d3333c
Elena Gachkova <99793003+LenaGachkova@users.noreply.github.com>
2022-05-24 13:09:41 +0300

    Update progress_of_work_spec.rb

--- LAB2/progress_of_work_spec.rb
+++ LAB2/progress_of_work_spec.rb
@@ -6,3 +6,9 @@ RSpec.describe "greeting" do
     expect(greeting("Иван", "Иванов", 7)).to eq "Привет, Иван Иванов. Тебе меньше 18 лет, но начать учиться программировать никогда не рано."
   end
 end
+
+RSpec.describe "foobar" do
+  it "choice a or b or sum" do
+    expect(foobar(20, 45)).to eq "45"
+  end
+end
