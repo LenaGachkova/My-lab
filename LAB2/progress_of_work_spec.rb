36a03a5fced5dcfe5af4fcc1592a15bd6cbc0b98
Elena Gachkova <99793003+LenaGachkova@users.noreply.github.com>
2022-05-26 11:24:30 +0300

    Update progress_of_work_spec.rb

--- LAB2/progress_of_work_spec.rb
+++ LAB2/progress_of_work_spec.rb
@@ -2,13 +2,13 @@ require 'rspec'
 require './progress_of_work.rb'
 
 RSpec.describe "greeting" do
-  it "should output the line" do
-    expect(greeting("Иван", "Иванов", 7)).to eq "Привет, Иван Иванов. Тебе меньше 18 лет, но начать учиться программировать никогда не рано."
+  it 'should output the line' do
+    expect(greeting("Иван", "Иванов", 19)).to eq "Привет, Иван Иванов. Самое время заняться делом."
   end
 end
 
 RSpec.describe "foobar" do
-  it "choice a or b or sum" do
-    expect(foobar(20, 45)).to eq "45"
+  it 'choice a or b or sum' do
+    expect(foobar(20, 45)).to eq "Второе число: 45"
   end
 end
